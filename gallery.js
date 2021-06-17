957f4fa881f8ce0bd514f8f498f8605fff5b328c
Vitalik Korobchuk <Vitalikkorobchuk@gmail.com>
2021-06-17 18:25:16 +0200

    Update code

--- gallery.js
+++ gallery.js
@@ -26,85 +26,87 @@ function createImages(images) {
   }).join('') 
 };
 
-//  Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
+// Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
 
-const modalOpen = document.querySelector('.lightbox');
-const modalImage = document.querySelector('.lightbox__image')
+const modalOpen = document.querySelector(".lightbox");
+const modalImage = document.querySelector(".lightbox__image");
 
-galleryOfImages.addEventListener('click', onImageClick);
+galleryOfImages.addEventListener("click", onImageClick);
 
-const originalImagesArrow = [];
-
-for (const item of galleryItems) {
-  originalImagesArrow.push(item.original);
-};
-
-let idxOfOpenedImage = originalImagesArrow.indexOf(modalImage.src);
+let idxOfOpenedImage = 0;
 
 function onImageClick(evt) {
-  if (!evt.target.classList.contains('gallery__image')) {
-    return
-  } else {
-    
+if (!evt.target.classList.contains("gallery__image")) {
+return;
+}
 // Открытие модального окна по клику на элементе галереи.
-    
-    modalOpen.classList.add('is-open');
-    
-    // Подмена значения атрибута src элемента img.lightbox__image.
-    modalImage.src = evt.target.dataset.source;
-    modalImage.alt = evt.target.alt;
 
-    // Пролистывание изображений галереи в открытом модальном окне клавишами "влево" и "вправо".
+modalOpen.classList.add("is-open");
 
-    window.addEventListener('keydown', onRightKeyClick);
-    window.addEventListener('keydown', onLeftKeyClick);
-  };
+// Подмена значения атрибута src элемента img.lightbox__image.
+idxOfOpenedImage = galleryItems.findIndex(
+(item) => item.original === evt.target.dataset.source
+);
+updateAttr(evt.target.dataset.source, evt.target.alt);
+// Пролистывание изображений галереи в открытом модальном окне клавишами "влево" и "вправо".
+
+window.addEventListener("keydown", onRightKeyClick);
+window.addEventListener("keydown", onLeftKeyClick);
 }
 
-const originalImagesLenght = originalImagesArrow.length-1;
+const originalImagesLenght = galleryItems.length - 1;
 
 function onRightKeyClick(evt) {
-  if (evt.keyCode === 39 && idxOfOpenedImage < originalImagesLenght) {
-      modalImage.src = originalImagesArrow[idxOfOpenedImage += 1]
-    }
-  }
+if (evt.keyCode === 39 && idxOfOpenedImage < originalImagesLenght) {
+getData((idxOfOpenedImage += 1));
+}
+}
 
 function onLeftKeyClick(evt) {
-  if (evt.keyCode === 37 && idxOfOpenedImage > 0) {
-      modalImage.src = originalImagesArrow[idxOfOpenedImage -= 1]
-    }
-  }
+if (evt.keyCode === 37 && idxOfOpenedImage > 0) {
+getData((idxOfOpenedImage -= 1));
+}
+}
+
+function getData(index) {
+const data = galleryItems[index];
+updateAttr(data.original, data.description);
+}
 
 // Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"].
 
-const onCloseBtnClick = document.querySelector('.lightbox__button');
+const onCloseBtnClick = document.querySelector(".lightbox__button");
 
-onCloseBtnClick.addEventListener('click', closeModal);
+onCloseBtnClick.addEventListener("click", closeModal);
 
 // Закрытие модального окна по клику на overlay.
 
-const onOverlayClick = document.querySelector('.lightbox__overlay');
+const onOverlayClick = document.querySelector(".lightbox__overlay");
 
-onOverlayClick.addEventListener('click', closeModal);
+onOverlayClick.addEventListener("click", closeModal);
 
 // Закрытие модального окна по клику на Esc.
 
-window.addEventListener('keydown', closeModalOnEscKeyClick);
+window.addEventListener("keydown", closeModalOnEscKeyClick);
 
 function closeModal() {
-  modalOpen.classList.remove('is-open');
-  window.removeEventListener('keydown', onRightKeyClick);
-  window.removeEventListener('keydown', onLeftKeyClick);
+modalOpen.classList.remove("is-open");
+window.removeEventListener("keydown", onRightKeyClick);
+window.removeEventListener("keydown", onLeftKeyClick);
 
-  // Очистка значения атрибута src элемента img.lightbox__image. 
-  // Это необходимо для того, чтобы при следующем открытии модального окна, пока грузится изображение, мы не видели предыдущее.
-  
-  modalImage.src = "";
-  modalImage.alt = "";
+// Очистка значения атрибута src элемента img.lightbox__image.
+// Это необходимо для того, чтобы при следующем открытии модального окна, пока грузится изображение, мы не видели предыдущее.
+
+updateAttr();
 }
 
 function closeModalOnEscKeyClick(evt) {
-  if (evt.keyCode === 27) {
-    closeModal();
-  }
+if (evt.keyCode === 27) {
+closeModal();
+}
+}
+
+function updateAttr(src = "", alt = "") {
+modalImage.src = src;
+modalImage.alt = alt;
 }
